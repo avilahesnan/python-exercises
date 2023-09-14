c125fae31f6572ddf3b9243990b9ec76a4a52bd4
Hesnan Ávila <avilahesnan@outlook.com>
2023-09-13 23:28:48 -0300

    new file: exe004

--- exercises/exe003.py
+++ exercises/exe003.py
@@ -1,26 +1,26 @@
-class Schedule:
+class Diary:
 
     list_store = []
 
     @classmethod
-    def print_schedule(cls):
-        return Schedule.list_store
+    def print_diary(cls):
+        return Diary.list_store
 
     @classmethod
     def print_person(cls, index):
-        return Schedule.list_store[index]
+        return Diary.list_store[index]
 
     @classmethod
     def search_person(cls, name):
-        for k, person in enumerate(Schedule.list_store):
+        for k, person in enumerate(Diary.list_store):
             if name == person[0]:
                 return f'{name} está na posição: {k}'
 
     @classmethod
     def remove_person(cls, name):
-        for k, person in enumerate(Schedule.list_store):
+        for k, person in enumerate(Diary.list_store):
             if name == person[0]:
-                Schedule.list_store.pop(k)
+                Diary.list_store.pop(k)
                 return f'{person[0]} foi excluído com sucesso!'
 
     def __init__(self, name, age, height):
@@ -29,17 +29,19 @@ class Schedule:
         self.__height = height
 
     def store_person(self):
-        Schedule.list_store.append([self.__name, self.__age, self.__height])
-        return print(f'{self.__name} foi adicionado com sucesso!')
+        if len(Diary.list_store) < 10:
+            Diary.list_store.append([self.__name, self.__age, self.__height])
+            return print(f'{self.__name} foi adicionado com sucesso!')
 
 
-user1 = Schedule('hesnan', 23, 175)
-user2 = Schedule('elon', 25, 171)
-user3 = Schedule('werik', 22, 180)
+user1 = Diary('hesnan', 23, 175)
+user2 = Diary('elon', 25, 171)
+user3 = Diary('werik', 22, 180)
 user1.store_person()
 user2.store_person()
 user3.store_person()
-print(Schedule.print_schedule())
+
+print(Diary.print_diary())
 print(user1.print_person(0))
 print(user1.search_person('elon'))
 print(user1.remove_person('werik'))
---
+++ exercises/exe004.py
@@ -0,0 +1,8 @@
+class Lift:
+
+    def __init__(self, current_floor, stories, capacity, quantity_people):
+        self.__current_floor = current_floor
+        self.__stories = stories
+        self.__capacity = capacity
+        self.__quantity_people = quantity_people
+
